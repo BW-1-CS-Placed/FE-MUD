0398299fca2abc3d4ab129c4fc84dae3e115b47b
Pat Palmerston <patpalmerston@gmail.com>
2020-03-03 09:28:51 -0800

    boiler plate redux ready for component

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 import {
     REGISTER_START,
     REGISTER_SUCCESS,
@@ -9,13 +10,51 @@ import {
 } from './types';
 
 // registration user
-export const registerUser = () => dispatch => {
-    console.log(dispatch);
+export const registerUser = register => dispatch => {
+    console.log('register user', register);
+    dispatch({ type: REGISTER_START });
+    return axios
+        .post(
+            'https://lambda-mud-test.herokuapp.com/api/registration',
+            register
+        )
+        .then(res => {
+            console.log(res);
+            localStorage.setItem('token', res.data.token);
+            localStorage.setItem('UserName', res.data.UserName);
+            dispatch({
+                type: REGISTER_SUCCESS,
+                payload: res.data.UserName
+            });
+        })
+        .catch(err => {
+            dispatch({
+                type: AUTH_ERROR,
+                payload: err.message
+            });
+        });
 };
 
 // login user
-export const loginUser = () => dispatch => {
-    console.log(dispatch);
+export const loginUser = creds => dispatch => {
+    console.log('login creds', creds);
+    dispatch({ type: LOGIN_START });
+    return axios
+        .post()
+        .then(res => {
+            localStorage.setItem('token', res.data.token);
+            localStorage.setItem('UserName', res.data.UserName);
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: res.data.UserName
+            });
+        })
+        .catch(err => {
+            dispatch({
+                type: AUTH_ERROR,
+                payload: err.message
+            });
+        });
 };
 
 // logout
