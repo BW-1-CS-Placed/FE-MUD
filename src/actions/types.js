74eb8affc203e76246111393e558800e81a029bc
Pat Palmerston <patpalmerston@gmail.com>
2020-03-03 10:47:52 -0800

    added boiler plate types for game and room actions

--- src/actions/types.js
+++ src/actions/types.js
@@ -7,3 +7,30 @@ export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGOUT = 'LOGOUT';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+
+// initial game
+export const INIT_START = 'INIT_START';
+export const INIT_SUCCESS = 'INIT_SUCCESS';
+export const INIT_FAILURE = 'INIT_FAILURE';
+
+// MOVEMENT
+export const MOVE_START = 'MOVE_START';
+export const MOVE_SUCCESS = 'MOVE_SUCCESS';
+export const MOVE_FAILURE = 'MOVE_FAILURE';
+
+// GET ROOMS
+export const GET_ROOM_START = 'GET_ROOM_START';
+export const GET_ROOM_SUCCESS = 'GET_ROOM_SUCCESS';
+export const GET_ROOM_FAILURE = 'GET_ROOM_FAILURE';
+
+// PLAYER
+export const PLAYER_START = 'PLAYER_START';
+export const PLAYER_SUCCESS = 'PLAYER_START';
+
+//MISC
+export const DESCRIPTION = 'DESCRIPTION';
+export const PLAYERS = 'PLAYERS';
+export const ERROR_MSG = 'ERROR_MSG';
+export const POSITION = 'POSITION';
+export const TITLE = 'TITLE';
