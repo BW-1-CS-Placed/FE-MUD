7a6ea123a5d8807f217a6d5fd4ba1a733b825c4f
Pat Palmerston <patpalmerston@gmail.com>
2020-03-03 19:24:57 -0800

    login and register displaying

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Login from './components/Login/Login'
-import Register from './components/Register/Register'
+//import client login component
+import Login from './components/Login/Login';
+//import client register component
+import Register from './components/Register/Register';
 import Navbar from './components/navBar/Navbar';
 import './App.css';
 
+// change components dependent on merge
 const App = () => {
     return (
         <Fragment>
@@ -13,12 +16,8 @@ const App = () => {
                 APP.js
                 <Switch>
                     <Route exact path='/' component={Login} />
-                    <Route
-                        exact
-                        path='/register'
-                        component={Register}
-                    />
-                    {/* <Route exact path='/game' component={World} /> */}
+                    <Route exact path='/register' component={Register} />
+                    {/* <Route exact path='/game' component={World} />  */}
                 </Switch>
             </div>
         </Fragment>
