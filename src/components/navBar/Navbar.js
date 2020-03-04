3b63f9965a7d50cabf5b845cd8febd6ec9ebe3de
chikechris <cchike2017@gmail.com>
2020-03-04 14:20:50 -0500

    added styles to navbar

--- src/components/navBar/Navbar.js
+++ src/components/navBar/Navbar.js
@@ -1,14 +1,33 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+// import styled from 'styled-components';
+import './navbar.scss';
+import mud from './mud.jpg';
 
 const Navbar = () => {
-    return (
-        <Fragment>
-            <Link to='/register'>Register</Link>
-            <Link to='/'>Login</Link>
-            {/* <Link to='/game'>Game</Link> */}
-        </Fragment>
-    );
+  return (
+    <div className='navbar'>
+      <div className='navbar-left'>
+        <img
+          src={mud}
+          alt='logo'
+          style={{
+            width: '50px',
+            height: '50px',
+            backgroundSize: 'contain'
+          }}
+        />
+        <Link to='#' className='links'>
+          Mud Games
+        </Link>
+      </div>
+      <div className='navbar-right'>
+        <Link to='/register'>Register</Link>
+        <Link to='/'>Login</Link>
+        {/* <Link to='/game'>Game</Link> */}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
